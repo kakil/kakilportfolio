6f0cfeccbb6648e586c30950f461656594cd1fe1
Kitwana Akil <kakil@me.com>
2023-04-26 17:18:17 -0400

    Completed framework for the Navbar.

--- src/sections/navbar/Navbar.jsx
+++ src/sections/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
 import './navbar.css'
+import Logo from '../../assets/logo.jpg'
+import data from './data'
+import {IoIosColorPalette} from 'react-icons/io'
 
 const Navbar = () => {
   return (
-    <nav id="navbar"><h2>NavBar</h2></nav>
+    <nav id="navbar">
+      <div className="container nav_container">
+        <a href="index.html" className='nav_logo'>
+          <img src={Logo} alt="Logo" />  
+        </a>
+        <ul className="nav_menu">
+          {
+            data.map(item => <li key={item.id}><a href={item.link}>{item.title}</a></li>)
+          }
+        </ul>
+        <button id='theme_icon'><IoIosColorPalette/></button>
+      </div>
+    </nav>
   )
 }
 
