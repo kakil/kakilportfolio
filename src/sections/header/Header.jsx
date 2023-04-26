294e08b39fd69fe9c3afe0872708b68103b712d2
Kitwana Akil <kakil@me.com>
2023-04-26 18:46:04 -0400

    Completed framework for header section.

--- src/sections/header/Header.jsx
+++ src/sections/header/Header.jsx
@@ -1,8 +1,29 @@
 import './header.css'
+import HeaderImage from '../../assets/header.jpg'
+import data from './data.js'
 
 const Header = () => {
   return (
-    <header id="header"><h2>HEADER</h2></header>
+    <header id="header">
+      <div className="container header_container">
+        <div className="header_profile">
+          <img src={HeaderImage} alt="Header Portrait" />
+        </div>
+        <h3>Kitwana Akil</h3>
+        <p>
+          You are a click away from building your dream website or web app.  Send me the details of your project for a modern, mobile responsive, highly performant website today!
+        </p>
+        <div className="header_cta">
+          <a href="#contact">Let's Talk</a>
+          <a href="#portfolio">My Work</a>
+        </div>
+        <div className="header_socials">
+          {
+            data.map(item => <a key={item.id} href={item.link} target="_blank" rel="noopener noreferrer">{item.icon}</a>)
+          }
+        </div>
+      </div>
+    </header>
   )
 }
 
---
+++ src/sections/header/data.js
@@ -0,0 +1,16 @@
+import {AiOutlineInstagram} from 'react-icons/ai'
+import {AiOutlineTwitter} from 'react-icons/ai'
+import {AiOutlineLinkedin} from 'react-icons/ai'
+import {AiFillGithub} from 'react-icons/ai'
+import {AiOutlineYoutube} from 'react-icons/ai'
+
+const data = [
+    {id: 1, link: 'https://www.instagram.com/akil69/', icon: <AiOutlineInstagram/>},
+    {id: 2, link: 'https://twitter.com/kakil', icon: <AiOutlineTwitter/>},
+    {id: 3, link: 'https://www.linkedin.com/in/kitwanaakil/', icon: <AiOutlineLinkedin/>},
+    {id: 4, link: 'https://github.com/kakil', icon: <AiFillGithub/>},
+    {id: 5, link: 'https://www.youtube.com/@kitwanaakil', icon: <AiOutlineYoutube/>},
+    
+]
+
+export default data
