3bc39ee2f0bbfbddd459d14778bf5e3b94930196
Kitwana Akil <kakil@me.com>
2023-04-26 21:00:43 -0400

    Completed initial About Me section code.

--- src/sections/about/About.jsx
+++ src/sections/about/About.jsx
@@ -1,8 +1,30 @@
 import './about.css'
+import AboutImage from './../../assets/about.jpg'
+import CV from './../../assets/cv.pdf'
+import {HiDownload} from 'react-icons/hi'
 
 const About = () => {
   return (
-    <section id="about"><h2>About</h2></section>
+    <section id="about">
+      <div className="container about__container">
+        <div className="about__left">
+          <div className="about__portrait">
+            <img src={AboutImage} alt="About Image" />
+          </div>
+        </div>
+        <div className="about__right">
+          <h2>About Me</h2>
+          <div className="about__cards"></div>
+          <p>
+            Building projects my clients love has always been my passion.  Being in the development industry for over 20 years and serving small businesses and large corporations worldwide.  I continue to be motivated to do my best!
+          </p>
+          <p>
+            Hi, my name is Kitwana Akil.  I'm from Jacksonville, Florida.  I'm a developer who has worked with iOS development, web development, artificial intelligence development, and I'm currently working with blockchain development.  My priority is to get your project up and running as soon as possible, and giving you industry-standard design and all the functionality you need so that your app or web site works the way you want it.  Get in touch today with the details of your project and let's get started!  Check out my resume below!
+          </p>
+          <a href={CV} download className='btn primary'>Download Resume <HiDownload/></a>
+        </div>
+      </div>
+    </section>
   )
 }
 
